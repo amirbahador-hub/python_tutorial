7114c6321794e1675c6cddf92e1d25c3d5b8123a
ManiMozaffar <mani.mozaffar@gmail.com>
2023-08-25 16:43:09 +0300

    🎨 Fix encapsulation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import asyncio
+from dataclasses import dataclass
 from enum import StrEnum, auto
 from functools import wraps
-from typing import Type
+from typing import Any, Type
 
 import aiohttp
 from aiohttp.client_exceptions import ClientError
@@ -26,10 +27,24 @@ class URLs(StrEnum):
     JsonPlaceHolder = "https://jsonplaceholder.typicode.com"
 
 
+@dataclass
+class ApiImpl:
+    _type: Type[Any]
+    sub_dirs: list[Any]
+
+
 class Response(BaseModel):
-    item: list[Item | None] | None = None
+    item: list[Any] | None = None
     excpt: Type[Exception] | None = None
     attempts: PositiveInt = 1
+
+
+STRATEGY: dict[URLs, ApiImpl] = {
+    URLs.JsonPlaceHolder: ApiImpl(
+        _type=list[Item | None],
+        sub_dirs=[Resources.posts, Resources.comments],
+    )
+}
 
 
 def retry(max_attempts: PositiveInt):
@@ -61,50 +76,51 @@ def create_url(pagination: int, resource: Resources, base_url: URLs) -> AnyUrl:
 async def send_request(
     session: aiohttp.ClientSession,
     url: AnyUrl,
+    _type=list[Any | None],
 ) -> Response:
     print(str(url))  # TODO: Change this to logging.debug
     async with session.get(str(url), verify_ssl=False) as response:
         response.raise_for_status()
-        items = TypeAdapter(list[Item | None]).validate_python(await response.json())
+        items = TypeAdapter(_type).validate_python(await response.json())
         return Response(item=items, excpt=None)
 
 
 async def start_fetching_cycle(
-    session: aiohttp.ClientSession, resource: Resources, base_url: URLs
+    session: aiohttp.ClientSession,
+    resource: Resources,
+    base_url: URLs,
+    _type=list[Any | None],
 ):
     result_had_items = True
     pagination = 0
     results: list[Response] = []
     while result_had_items:
         pagination += 1
-        url = create_url(
-            resource=resource,
-            pagination=pagination,
-            base_url=base_url,
-        )
-        task = asyncio.create_task(send_request(session=session, url=url))
+        url = create_url(resource=resource, pagination=pagination, base_url=base_url)
+        task = asyncio.create_task(send_request(session=session, url=url, _type=_type))
         result = await task
         result_had_items = result.item and len(result.item) > 0
         results.append(result)
     return results
 
 
-async def main():
-    resources: list[Resources] = [Resources.posts, Resources.comments]
+async def main(to_fetch: URLs):
+    strategy = STRATEGY.get(to_fetch)
+    assert strategy is not None
 
     async with aiohttp.ClientSession() as session:
         tasks = list()
-        for resource in resources:
+        for resource in strategy.sub_dirs:
             task = asyncio.create_task(
                 start_fetching_cycle(
                     session=session,
                     resource=resource,
                     base_url=URLs.JsonPlaceHolder,
+                    _type=strategy._type,
                 )
             )
             tasks.append(task)
-        result = await asyncio.gather(*tasks)
-        print(f"Finished fetching -> {result}")
+        await asyncio.gather(*tasks)
 
 
-asyncio.run(main())
+asyncio.run(main(URLs.JsonPlaceHolder))
