09dde417c84df6b2332aaf41b2e78717ef6bc522
Sadegh Yazdani <silverstar10@gmail.com>
2023-08-25 22:10:34 +0330

    ♻️ Add retry function and separate some parts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import AsyncGenerator
+from typing import AsyncGenerator, Callable
 from dataclasses import dataclass
 import logging
 import aiohttp
@@ -10,9 +10,18 @@ class Item:
     id: int
     body: str
 
-class JsonplaceholderAPI:
 
-    base_url = 'https://jsonplaceholder.typicode.com/'
+def retry(func: Callable, retries: int, *args, **kwargs):
+    for i in range(retries):
+        try:
+            return func(*args, **kwargs)
+        except Exception:
+            continue
+    return None
+
+
+class JsonplaceholderAPI:
+    base_url = "https://jsonplaceholder.typicode.com/"
 
     def __init__(self, session: aiohttp.ClientSession):
         self.session = session
@@ -20,15 +29,12 @@ class JsonplaceholderAPI:
     async def fetch_with_retry(
         self, url: str, retries: int
     ) -> aiohttp.ClientResponse | None:
-        async with self.session.get(
-            url
-        ) as response:
-            try:
-                for i in range(retries):
-                    return await response.json()
-            except Exception:
-                return None
-        
+        async def get(session, url):
+            async with session.get(url) as response:
+                res = await response.json()
+            return res
+
+        return await retry(get, retries, self.session, url)
 
     async def retrieve_data(
         self,
@@ -39,45 +45,40 @@ class JsonplaceholderAPI:
         try:
             items = await self.fetch_with_retry(url, retries=10)
             for item in items:
-                item_id = item.get("id")
-                item_body = item.get("body")
-                yield Item(
-                    id=item_id,
-                    body=item_body,
-                )
+                yield self.create_item(item)
             if items is not None:
-                    async for item in self.retrieve_data(
-                        resource=resource, pagination=pagination + 1
-                    ):
-                        yield item
+                async for item in self.retrieve_data(
+                    resource=resource, pagination=pagination + 1
+                ):
+                    yield item
         except Exception as ex:
-            logging.error(
-                f"Exception: {ex}"
-            )
+            logging.error(f"Exception: {ex}")
+
+    @staticmethod
+    def create_item(item):
+        item_id = item.get("id")
+        item_body = item.get("body")
+        return Item(
+            id=item_id,
+            body=item_body,
+        )
 
 
 async def process_items(
     api: JsonplaceholderAPI,
     resource: str,
 ) -> None:
-    async for item in api.retrieve_data(
-        resource=resource
-    ):
+    async for item in api.retrieve_data(resource=resource):
         print(item)
 
 
 async def main():
     reses = ["posts", "comments"]
     async with aiohttp.ClientSession() as session:
-        tasks = list()
-
         api = JsonplaceholderAPI(session=session)
-        for res in reses:
-            task = asyncio.create_task(process_items(
-                api,
-                resource=res
-            ))
-            tasks.append(task)
+        tasks = [asyncio.create_task(process_items(api, resource=res)) for res in reses]
+
         await asyncio.gather(*tasks)
+
 
 asyncio.run(main())
