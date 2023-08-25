36c81dd626aa47a4e6f34417b9a05da24b7bf38e
farhadBM <farhadleon2019@gmail.com>
2023-08-26 01:20:21 +0400

    code refactor
    -Pagination is moved out from JsonPlaceHolderApi
    -Request class implemented
    -For Pagination ending a custom exception is implemented
    -Some Decoupling

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from typing import AsyncGenerator
-from dataclasses import dataclass
-import logging
-import aiohttp
+from typing import AsyncGenerator, Any
+import json
 import asyncio
+import logging
+from dataclasses import dataclass
+
+import aiohttp
+from aiohttp.client_exceptions import ClientError, ContentTypeError
+
+
+class PaginationEndedException(Exception):
+    pass
+
+
+class FetchingDataFailedException(Exception):
+    pass
+
+
+class JsonParsingException(Exception):
+    pass
 
 
 @dataclass
@@ -11,72 +26,81 @@ class Item:
     body: str
 
 
-class JsonplaceholderAPI:
-    base_url = 'https://jsonplaceholder.typicode.com/'
-
+class Request:
     def __init__(self, session: aiohttp.ClientSession):
         self.session = session
 
-    async def fetch_with_retry(
-            self, url: str, retries: int
-    ) -> dict | None:
+    async def get_with_retry(self, url: str, retries: int = 1) -> aiohttp.ClientResponse:
+        for _ in range(retries):
+            try:
+                response = await self.session.get(url)
+                return response
+            except ClientError as error:
+                logging.info(error)
+
+        raise FetchingDataFailedException()
+
+
+class JsonPlaceholderApi:
+    base_url = 'https://jsonplaceholder.typicode.com/'
+
+    def __init__(self, request: Request):
+        self.request = request
+
+    async def retrieve_data(self, resource: str, pagination: int = 1) -> AsyncGenerator[Item, None]:
+        url = self.base_url + f"{resource}?_page={pagination}"
+        place_holder_response = await self.request.get_with_retry(url, retries=5)
+        items = await self.extract_data(client_response=place_holder_response)
+        for item in items:
+            yield self.create_item(item)
+
+    async def extract_data(self, client_response: aiohttp.ClientResponse) -> list[dict]:
+        """if the json response can't be parsed raise PaginationEndedException"""
+        items = await self.get_json(client_response)
+        if len(items) > 0:
+            return items
+        else:
+            raise PaginationEndedException()
+
+    async def get_json(self, client_response: aiohttp.ClientResponse) -> Any:
         try:
-            for i in range(retries):
-                async with self.session.get(url) as response:
-                    return await response.json()
-        except Exception:
+            return await client_response.json()
+        except (json.JSONDecodeError, ContentTypeError):
+            raise JsonParsingException()
+
+    def create_item(self, item: dict) -> Item | None:
+        try:
+            return Item(id=item.get('id'), body=item.get('body'))
+        except AttributeError:
             return None
 
-    async def retrieve_data(
-            self,
-            resource: str,
-            pagination: int = 1,
-    ) -> AsyncGenerator[Item, None]:
-        url = self.base_url + f"{resource}?_page={pagination}"
+
+async def process_items_with_pagination(resource: str, api: JsonPlaceholderApi):
+    pagination = 0
+    while True:
+        pagination += 1
         try:
-            items = await self.fetch_with_retry(url, retries=10)
-            print(items)
-            for item in items:
-                item_id = item.get("id")
-                item_body = item.get("body")
-                yield Item(
-                    id=item_id,
-                    body=item_body,
-                )
-            if items is not None:
-                async for item in self.retrieve_data(
-                        resource=resource, pagination=pagination + 1
-                ):
-                    yield item
-        except Exception as ex:
-            logging.error(
-                f"Exception: {ex}"
-            )
-
-
-async def process_items(
-        api: JsonplaceholderAPI,
-        resource: str,
-) -> None:
-    async for item in api.retrieve_data(
-            resource=resource
-    ):
-        print(item)
+            async for item in api.retrieve_data(resource, pagination):
+                print(item)
+        except PaginationEndedException:
+            break
+        except (FetchingDataFailedException, JsonParsingException):
+            continue
 
 
 async def main():
-    reses = ["posts", "comments"]
-    async with aiohttp.ClientSession() as session:
-        tasks = list()
+    resources = ["posts", "comments"]
 
-        api = JsonplaceholderAPI(session=session)
-        for res in reses:
-            task = asyncio.create_task(process_items(
-                api,
-                resource=res
-            ))
-            tasks.append(task)
+    tasks = list()
+    async with aiohttp.ClientSession() as session:
+        request = Request(session=session)
+        api = JsonPlaceholderApi(request=request)
+        for resource in resources:
+            tasks.append(
+                asyncio.create_task(process_items_with_pagination(resource=resource, api=api))
+            )
         await asyncio.gather(*tasks)
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
